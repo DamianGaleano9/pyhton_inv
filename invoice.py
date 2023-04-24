6032977f8058d00337ba8fa5b80e6ef41a728836
Damian Galeano <dj.damian.b@hotmail.com>
2023-04-24 13:01:09 +0200

    Add getter and setter

--- invoice.py
+++ invoice.py
@@ -1,19 +1,30 @@
-class Invoice: 
+class Invoice:
 
     def __init__(self, client, total):
-        self.client = client
-        self.total = total
+        self._client = client
+        self._total = total
 
     def formatter(self):
-        return f'{self.client} owes {self.total}'
-    
+        return f'{self._client} owes {self._total}'
+
+    @property
+    def client(self):
+        return self._client
+
+    @client.setter
+    def client(self, client):
+        self._client = client
+
+    @property
+    def total(self):
+        return self._total
+
+
 google = Invoice('Google', 100)
-instagram = Invoice('Instragam', 200)
-facebook = Invoice('Facebook', 200)
 
 
-# print(google.formatter())
-# print(instagram.formatter())
-
-print(google.client)
 print(google.total)
+
+
+google.client = 'Instagram'
+print(google.client)
