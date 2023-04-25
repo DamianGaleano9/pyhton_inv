cc08e3d1a6d2fb4b5384b84d760b095a188ffbb1
Damian Galeano <dj.damian.b@hotmail.com>
2023-04-25 17:28:27 +0200

    Working

--- iterator.py
+++ iterator.py
@@ -1,3 +1,22 @@
+class Lineup:
+    def __init__(self, players):
+        self.players = players
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n < (len(self.players) - 1):
+            player = self.players[self.n]
+            self.n += 1
+            return player
+        elif self.n == (len(self.players) - 1):
+            player = self.players[self.n]
+            self.n = 0
+            return player
+
+
 astros = [
     'Springer',
     'Bregman',
@@ -8,7 +27,40 @@ astros = [
     'MacCann'
     'Davis',
     'Tucker'
+
+
 ]
 
-astros.append('Damian')
-print(astros)
+astros_lineup = Lineup(astros)
+process = iter(astros_lineup)
+
+
+
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+
+
