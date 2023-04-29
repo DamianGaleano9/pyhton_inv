ba9d80cfc3aa4bb19e6771a671dc0f42972fa742
Damian Galeano <dj.damian.b@hotmail.com>
2023-04-29 13:22:06 +0200

    get dict

---
+++ website.py
@@ -0,0 +1,19 @@
+# class Website:
+#     def __init__(self, title):
+#         self.title = title  
+
+# ws = Website('My website is working')
+# print(ws.__dict__)
+
+# ws_two = Website('My second website is here')
+# print(ws_two.__dict__)
+
+
+class DifferentWebsite:
+    title  = 'My class title'
+
+dw = DifferentWebsite()
+print(dw.title)
+
+dm = DifferentWebsite()
+print(dm.title)
